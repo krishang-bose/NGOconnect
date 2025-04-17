3fd741e5618380b30b2e8351eccbf62191d92181
Krishang <144909920+krishang-bose@users.noreply.github.com>
2025-04-17 11:36:52 +0530

    created rough backend

---
+++ backend/src/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "../../routes/auth.js";
+import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
+import messageRoutes from "../../routes/message.js";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
+
+app.use("/api/auth", authRoutes);
+app.use("/api/message",messageRoutes);
+
+dotenv.config();
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+    console.log("Server is running on port " + PORT);
+    connectDB();
+});
