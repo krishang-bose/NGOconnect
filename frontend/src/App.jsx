4155414862baaf662f3a52aa89c1efc8aea694d5
Krishang <144909920+krishang-bose@users.noreply.github.com>
2025-04-21 01:31:00 +0530

    wuhu

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,42 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import './index.css';
 import SignupPage from './pages/SignUp';
 import LoginPage from './pages/LoginPage';
 import AboutUs from './pages/AboutUs';
 import BlogPage from './pages/BlogSpot';
+import { useAuthStore } from './store/useAuthStore';
 import NGOSearchPage from './pages/NGOSearchPage';
+import { useEffect } from 'react';
+import { Loader } from 'lucide-react';
+import { Toaster } from 'react-hot-toast';
+
 
 function App() {
+  const { authUser, checkAuth, isCheckingAuth} = useAuthStore();
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+  console.log({authUser});
+  if(isCheckingAuth && authUser === null){ return (
+    <div className="flex items-center justify-center h-screen">
+      <Loader className="size-10 animate-spin" />
+    </div>
+  )}
   return (
+  <div>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage/>} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/aboutus" element={<AboutUs />} />
-        <Route path="/blog" element={<BlogPage/>} />
-        <Route path="/ngos" element={<NGOSearchPage/>} />
+        <Route path="/" element={authUser?<HomePage />: <Navigate to= "login/"/>} />
+        <Route path="/signup" element={!authUser? <SignupPage />: <Navigate to="/"/>} />
+        <Route path="/login" element={!authUser? <LoginPage />: <Navigate to="/"/>} />
+
+        <Route path="/aboutus" element={authUser?<AboutUs />: <Navigate to="login/"/>} />
+        <Route path="/blog" element={authUser?<BlogPage />: <Navigate to="login/"/>} />
+        <Route path="/ngos" element={authUser?<NGOSearchPage />: <Navigate to="login/"/>} />
       </Routes>
+    <Toaster/>
+  </div>
   );
 }
 
