2cb8e8b4abfa2aaa225f7682c1a407a84d97e757
Krishang <144909920+krishang-bose@users.noreply.github.com>
2025-04-22 02:06:48 +0530

    removed navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,7 +14,6 @@ import { Loader } from 'lucide-react';
 import { Toaster } from 'react-hot-toast';
 import Dashboard from './pages/Dashboard';
 import DonationPage from './pages/Donation';
-import NavBar from './components/NavBar';
 
 
 function App() {
@@ -31,7 +30,6 @@ function App() {
   )}
   return (
   <div>
-    {!hideNavBar && <NavBar />}
       <Routes>
         <Route path="/" element={authUser?<HomePage />: <Navigate to= "login/"/>} />
         <Route path="/signup" element={!authUser? <SignupPage />: <Navigate to="/"/>} />
